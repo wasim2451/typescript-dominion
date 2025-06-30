6c1710069ce0a5e4cb136e605d9e60fbb0037c4e
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-06-30 16:53:44 +0530

    Module 1 : Learnt how to fetch data from API normally

--- index.ts
+++ index.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
  async function fetchUsers(){
     const res=await axios.get('https://jsonplaceholder.typicode.com/users');
-    console.log(res.data);
+    // console.log(res.data);
+    const data=res.data;
+    for(let i=0;i<3;i++){
+        console.log(data[i].name);
+    }
  }
  fetchUsers();
